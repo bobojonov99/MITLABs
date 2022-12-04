05955a88dec769737e220f6f35b7932d8454fdf2
Amirjon Bobojonov <bobojonov.amirjon99@gmail.com>
2022-12-05 00:32:38 +0300

    fix lab3

--- lab3/main.py
+++ lab3/main.py
@@ -2,14 +2,15 @@ from scipy.io import wavfile
 from sklearn.decomposition import PCA
 import numpy as np
 
-samplerate, wav = wavfile.read('comare-icontinue.wav')  # cчитывание музыки в формаnt wav
+samplerate, audio_file = wavfile.read('comare-icontinue.wav')  # cчитывание музыки в формаnt wav
+signal_wav = audio_file[:, 0] #заполнения считанного сигнала нулями
 
 
 def pca_reduce(signal, n_components, block_size=1024):
     # дополнение сигнал нулями, для того,чтобы он делился на block_size
     samples = len(signal)
-    hanging = block_size - np.mod(samples, block_size)
-    padded = np.lib.pad(signal, (0, hanging), 'constant', constant_values=0)
+    hanging = block_size - np.mod(samples, block_size) #остаток от деления
+    padded = np.lib.pad(signal, (0, hanging), 'constant', constant_values=0) #заполения нулями
     # изменение формы сигнала на размерность 1024
     reshaped = padded.reshape((len(padded) // block_size, block_size))
     # Метод главных компонент
@@ -20,7 +21,6 @@ def pca_reduce(signal, n_components, block_size=1024):
     return pca, transformed, reconstructed
 
 
-wav_left = wav[:, 0]
-reconstructed = pca_reduce(wav_left, 200, 1024)
+reconstructed = pca_reduce(signal_wav, 250, 1024)
 wavfile.write("comare-icontinue_After_PCA.wav", samplerate,
               reconstructed[2].astype(np.int16))  # запись полученного результата
